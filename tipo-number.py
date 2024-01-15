0d38c6b4122e38f122f40398f5d71730790e804e
Ruan Araújo <ruands2002@gmail.com>
2024-01-15 18:41:23 -0300

    aula4: tipos numéricos

---
+++ tipo-number.py
@@ -0,0 +1,23 @@
+"""
+Tipos int e float
+int -> integer -> número inteiro
+float -> float -> número flutuante
+
+O tipo int representa qualquer número positivo ou negativo.
+O tipo float representa qualquer número positivo ou negativo com ponto flutuante.
+
+A função type mostra o tipo que o python inferiu ao valor.
+"""
+ 
+# Int
+print(11)
+print(-11)
+print(0)
+
+# Float
+print(11.1)
+print(-11.1)
+print(0.0)
+
+print(type(1))
+print(type(1.0))
