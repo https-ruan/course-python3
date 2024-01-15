d34033f60feaaf07f2d7a927d90517fc0cdceadc
Ruan Araújo <ruands2002@gmail.com>
2024-01-15 20:14:29 -0300

    aula14: método format

--- formatacao-strings.py
+++ formatacao-strings.py
@@ -3,9 +3,17 @@ altura = 1.67
 peso = 80
 imc = peso / (altura * altura)
 
-
+# * f-strings
 linha_1 = f'{nome} tem {altura: .2f} de altura'
 linha_2 = f'pesa {peso}, quilos e seu IMC é{imc: .2f}'
 
 print(linha_1)
 print(linha_2)
+
+# * função format()
+a = 'AAA'
+b = 'BBBB'
+c = 1.1
+string = 'a={0}, b={1}, c={nome3: .2f}'
+formato = string.format(a, b, nome3 = c)
+print(formato)
