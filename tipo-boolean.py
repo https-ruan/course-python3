572dff661c7cd058156493f370dbbf2c22179c80
Ruan Araújo <ruands2002@gmail.com>
2024-01-15 18:46:38 -0300

    aula5: tipo boolean

---
+++ tipo-boolean.py
@@ -0,0 +1,15 @@
+"""
+Tipo bool
+bool -> boolean -> verdadeiro ou falso
+
+Ao questionar algo em um programa, só existem duas respostas possíveis:
+sim (True) ou não (False).
+
+Existem vários operadores para "questionar"
+
+Operadores: == 
+"""
+ 
+print(10 == 10)
+print(10 == 11)
+print(type(10 == 11))
