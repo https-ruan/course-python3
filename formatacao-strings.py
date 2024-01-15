6213f796676a6af531a0aa0d6182c384be5aab98
Ruan Araújo <ruands2002@gmail.com>
2024-01-15 19:55:32 -0300

    aula13: introducao a f-strings

---
+++ formatacao-strings.py
@@ -0,0 +1,11 @@
+nome = 'Ruan'
+altura = 1.67
+peso = 80
+imc = peso / (altura * altura)
+
+
+linha_1 = f'{nome} tem {altura: .2f} de altura'
+linha_2 = f'pesa {peso}, quilos e seu IMC é{imc: .2f}'
+
+print(linha_1)
+print(linha_2)
