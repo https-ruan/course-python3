5c5e0b5ee2f672efb3b99b95f91215b029226312
Ruan Araújo <ruands2002@gmail.com>
2024-01-15 18:34:41 -0300

    aula3: tipo string

---
+++ tipo-string.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+ 
+# Aspas simples
+print('Ruan Araujo')
+print('Ruan "Araujo"') # Para melhor leitura
+
+# Aspas duplas
+print("Ruan Araujo")
+print("Ruan 'Araujo'") # Para melhor leitura
+
+# Escape -> \
+print("Ruan \"Araujo\"")
+
+# r
+print(r"Ruan \"Araujo\"")
