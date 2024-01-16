44ee42a401bd768e8f8aca82be745980af66cf74
Ruan Araújo <ruands2002@gmail.com>
2024-01-16 13:53:05 -0300

    aula25: interpolação básica com %

--- formatacao-strings.py
+++ formatacao-strings.py
@@ -1,19 +1,29 @@
-nome = 'Ruan'
-altura = 1.67
-peso = 80
-imc = peso / (altura * altura)
-
 # * f-strings
-linha_1 = f'{nome} tem {altura: .2f} de altura'
-linha_2 = f'pesa {peso}, quilos e seu IMC é{imc: .2f}'
+# nome = 'Ruan'
+# altura = 1.67
+# peso = 80
+# imc = peso / (altura * altura)
+# linha_1 = f'{nome} tem {altura: .2f} de altura'
+# linha_2 = f'pesa {peso}, quilos e seu IMC é{imc: .2f}'
 
-print(linha_1)
-print(linha_2)
+# print(linha_1)
+# print(linha_2)
 
 # * função format()
-a = 'AAA'
-b = 'BBBB'
-c = 1.1
-string = 'a={0}, b={1}, c={nome3: .2f}'
-formato = string.format(a, b, nome3=c)
-print(formato)
+# a = 'AAA'
+# b = 'BBBB'
+# c = 1.1
+# string = 'a={0}, b={1}, c={nome3: .2f}'
+# formato = string.format(a, b, nome3=c)
+# print(formato)
+
+# * interpolação básica com %
+# s - string
+# d / i - int
+# f - float
+# x / X - Hexadecimal (ABCDEF0123456789)
+nome = 'Ruan'
+preco = 1000.92131342
+variavel = '%s, o preço total foi R$%.2f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %08X' % (1523, 1523))
