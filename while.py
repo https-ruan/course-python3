f079b2f38dee404f7070cbd2f4602826dd32a012
Ruan Araújo <ruands2002@gmail.com>
2024-01-17 21:49:59 -0300

    aula37: while + palavra 'continue'

--- while.py
+++ while.py
@@ -3,10 +3,26 @@ Repetiçõews
 while (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
 Loop infinito -> Quando um código não tem fim
+
+break -> finaliza o laço de repetição
+continue -> pula o restante do bloco do laço e retorna ao inicio
 """
 
 contador = 0
 
-while contador < 10:
+while contador < 100:
     contador += 1
+
+    if contador == 6:
+        print('Não vou mostrar o 6.')
+        continue
+
+    if contador >= 10 and contador <= 27:
+        continue
+
     print(contador)
+
+    if contador == 40:
+        break
+
+print('Acabou')
