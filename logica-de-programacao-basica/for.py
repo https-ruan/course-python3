627bdce9d45a4c9f433ffe7ed224bafaaa81efc0
Ruan Araújo <ruands2002@gmail.com>
2024-01-17 23:52:59 -0300

    aula44: for + range

--- logica-de-programacao-basica/for.py
+++ logica-de-programacao-basica/for.py
@@ -1,4 +1,15 @@
-texto = 'Python'
+# texto = 'Python'
 
-for letra in texto:
-    print(letra)
+# for letra in texto:
+#     print(letra)
+
+
+""" 
+for + range 
+range -> range(start, stop, step)
+"""
+numeros = range(10)
+numeros = range(5, 10)
+numeros = range(5, 10, 2)
+for numero in numeros:
+    print(numero)
