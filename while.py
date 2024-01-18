5e2c538f0c4913b62cb4ed9b6296b00fa15fff73
Ruan Araújo <ruands2002@gmail.com>
2024-01-17 21:35:18 -0300

    aula35: operador while

---
+++ while.py
@@ -0,0 +1,12 @@
+""" 
+Repetiçõews
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+
+contador = 0
+
+while contador < 10:
+    contador += 1
+    print(contador)
