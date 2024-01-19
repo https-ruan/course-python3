53c238b540207e08f8d4dd37d5a6798734971f3f
Ruan Araújo <ruands2002@gmail.com>
2024-01-18 21:48:54 -0300

    aula47: tipo list

---
+++ logica-de-programacao-basica/tipo-list.py
@@ -0,0 +1,19 @@
+""" 
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, inser, pop, del, clear, extend, +
+"""
+
+#         01234
+#        -54321
+string = 'ABCDE'  # 5 caracteres
+# print(bool([])) # falsy
+# print(lista, type(lista))
+
+#        0    1      2             3
+#       -4   -3     -2            -1
+lista = [123, True, 'Ruan Araujo', 1.2]
+lista[-3] = 'Maria'
+print(lista[1], type(lista[2]))
