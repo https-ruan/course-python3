04a5e5fb2f448a2a04bc916f7556dad309b3360c
Ruan Araújo <ruands2002@gmail.com>
2024-01-19 13:29:48 -0300

    aula54: tuplas

--- logica-de-programacao-basica/desenpacotamento-tuplas.py
+++ logica-de-programacao-basica/desenpacotamento-tuplas.py
@@ -1,6 +1,13 @@
 """ 
 Introdução ao desempacotamento + tuples (tuplas)
+
+Tipo tupla -> Uma lista imutável
 """
 _, nome2, *_ = ['Maria', 'Helena', 'Ruan']
-
 print(nome2, _)
+
+# * tupla
+tupla = ('Maria', 'Helena', 'Ruan')
+tupla = tuple(['Maria', 'Helena', 'Ruan'])
+lista = list(tupla)
+print(tupla)
