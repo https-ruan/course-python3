0365f13fa4370ba98f98cdd5e5de91c60ffee01a
Ruan Araújo <ruands2002@gmail.com>
2024-01-18 22:14:23 -0300

    aula50: concatenando e estendendo listas

--- logica-de-programacao-basica/tipo-list.py
+++ logica-de-programacao-basica/tipo-list.py
@@ -38,6 +38,14 @@ Métodos úteis:
 # print(lista)
 
 # * Inserindo itens em qualquer índice
-lista = [1, 2, 3, 4]
-lista.insert(0, 5)
-print(lista)
+# lista = [1, 2, 3, 4]
+# lista.insert(0, 5)
+# print(lista)
+
+# * Concatenando e estendendo listas
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+lista_a.extend(lista_b)
+print(lista_c)
+print(lista_a)
