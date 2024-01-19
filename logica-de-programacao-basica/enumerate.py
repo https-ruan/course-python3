eb2852dc52eb3c0b0268dd57f29c7d2029b93777
Ruan Araújo <ruands2002@gmail.com>
2024-01-19 13:39:34 -0300

    aula55: enumerador

---
+++ logica-de-programacao-basica/enumerate.py
@@ -0,0 +1,8 @@
+""" 
+enumerate - enumera iteráveis (índices)
+"""
+# [(0, 'Maria'), (1, 'Helena'), (2, 'Ruan')]
+lista = ['Maria', 'Helena', 'Ruan']
+
+for indice, nome in enumerate(lista):
+    print(indice, nome)
