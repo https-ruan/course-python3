adb600ecc759523abb11deaa9af326b216c24e30
Ruan Araújo <ruands2002@gmail.com>
2024-01-21 15:53:34 -0300

    aula58: iteraveis dentro de iteraveis

---
+++ logica-de-programacao-basica/lista-dentro-listas.py
@@ -0,0 +1,12 @@
+salas = [
+    # 0        1
+    ['Maria', 'Helena'],  # 0
+    # 0
+    ['Elaine'],  # 1
+    # 0        1        2
+    ['Luiz', 'João', 'Eduarda', (0, 10, 20, 30, 40)]  # 2
+]
+
+print(salas[0][1])
+print(salas[2][2])
+print(salas[2][3][2])
