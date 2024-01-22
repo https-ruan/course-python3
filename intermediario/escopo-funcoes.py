95958bd5d27d05990d0807e1dbe42935c8f9d3d8
Ruan Araújo <ruands2002@gmail.com>
2024-01-22 16:41:23 -0300

    intermediario: escopos de código #2

--- intermediario/escopo-funcoes.py
+++ intermediario/escopo-funcoes.py
@@ -3,6 +3,7 @@ Escopo significa o local onde aquele código pode atingir.
 Existe o escopo global e local.
 O escopo global é o escopo onde todo o código é alcançavel.
 O escopo local é o escopo onde apenas nomes do mesmo local podem ser alcançados.
+! A palavra 'global' faz uma variável do escopo externo ser a mesma do escopo interno.
 """
 
 x = 1
