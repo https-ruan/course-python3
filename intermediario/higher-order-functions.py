d696fef35e24639fb4a9dd324d9417460b26e112
Ruan Araújo <ruands2002@gmail.com>
2024-01-22 21:58:38 -0300

    intermediario: higher order functions

---
+++ intermediario/higher-order-functions.py
@@ -0,0 +1,19 @@
+""" 
+Higher Order Functions
+Funções de primeira classe
+
+Higher Order Functions - Funções que podem receber e/ou retornar outras funções
+
+First-Class Functions - Funções que são tratadas como outros tipos de dados comuns (strings, inteiros, etc...)
+"""
+
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}!'
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+print(executa(saudacao, 'Olá', 'Ruan'))
