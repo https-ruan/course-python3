4d7f93034a14b671fb0ace29f664f9d7978d9e3f
Ruan Araújo <ruands2002@gmail.com>
2024-01-22 22:53:04 -0300

    intermediário: shallow copy vs deep copy

--- intermediario/metodos-dict.py
+++ intermediario/metodos-dict.py
@@ -12,13 +12,16 @@ pop -> Apaga um item com a chave especificada (del)
 popitem -> Apaga o último item adicionado
 update -> Atualiza um dicionário com outro
 """
+import copy
+
 pessoa = {
     'nome': 'Luiz Otávio',
     'sobrenome': 'Miranda',
-    'idade': 900
+    'idade': 900,
+    'lista': [1, 2, 3]
 }
 
-pessoa.setdefault('idade', None)
+# pessoa.setdefault('idade', None)
 
 # print(len(pessoa))
 # print(pessoa.keys())
@@ -30,5 +33,12 @@ pessoa.setdefault('idade', None)
 # for valor in pessoa.items():
 #     print(valor)
 
-for chave, valor in pessoa.items():
-    print(chave, valor)
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
+# pessoa2 = pessoa.copy()
+pessoa2 = copy.deepcopy(pessoa)
+pessoa2['nome'] = 'Ruan Araujo'
+pessoa2['lista'][0] = 12
+print(pessoa2)
+print(pessoa)
