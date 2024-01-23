172ad5eb68b17dc36fd003fe0f0cb956b036bb4a
Ruan Araújo <ruands2002@gmail.com>
2024-01-22 22:58:51 -0300

    intermediário: métodos do tipo dicionário #2

--- intermediario/metodos-dict.py
+++ intermediario/metodos-dict.py
@@ -28,6 +28,20 @@ pessoa = {
 # print(pessoa.values())
 # print(pessoa.items())
 # print(pessoa['idade'])
+# print(pessoa.get('nome'))
+
+# print(pessoa.pop('nome'))
+# print(pessoa)
+# print(pessoa.popitem())
+# print(pessoa)
+
+# pessoa.update({
+#     'lista2': [4, 5, 6]
+# })
+# pessoa.update(lista2=[4, 5, 6])
+# pessoa.update((('lista3', [7, 8, 9]), ('idade', 22)))
+# pessoa.update([['lista3', [7, 8, 9]], ['idade', 22]])
+print(pessoa)
 
 
 # for valor in pessoa.items():
@@ -36,9 +50,10 @@ pessoa = {
 # for chave, valor in pessoa.items():
 #     print(chave, valor)
 
+# * Shallow copy e Deep copy
 # pessoa2 = pessoa.copy()
-pessoa2 = copy.deepcopy(pessoa)
-pessoa2['nome'] = 'Ruan Araujo'
-pessoa2['lista'][0] = 12
-print(pessoa2)
-print(pessoa)
+# pessoa2 = copy.deepcopy(pessoa)
+# pessoa2['nome'] = 'Ruan Araujo'
+# pessoa2['lista'][0] = 12
+# print(pessoa2)
+# print(pessoa)
