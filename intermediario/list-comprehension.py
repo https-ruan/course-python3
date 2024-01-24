f215080e403558a95b23bf1777cfae06d66b4d3f
Ruan Araújo <ruands2002@gmail.com>
2024-01-24 12:02:02 -0300

    intermediário: mapeamento de dados em list comprehension (map)

--- intermediario/list-comprehension.py
+++ intermediario/list-comprehension.py
@@ -1,14 +1,29 @@
-# List comprehension é uma forma rápida para criar listas a partir de iteráveis.
+
+# * List comprehension é uma forma rápida para criar listas a partir de iteráveis.
 
 # print(list(range(10)))
 
-lista = []
-for numero in range(10):
-    lista.append(numero)
+# lista = []
+# for numero in range(10):
+#     lista.append(numero)
 # print(lista)
 
 
-lista = [
-    numero * 2 for numero in range(10)
+# lista = [
+#     numero * 2 for numero in range(10)
+# ]
+# print(lista)
+
+# * Mapeamento de dados em list comprehension
+produtos = [
+    {'nome': 'p1', 'preco': 10},
+    {'nome': 'p2', 'preco': 20},
+    {'nome': 'p3', 'preco': 30}
 ]
-print(lista)
+
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
+print(*novos_produtos, sep='\n')
