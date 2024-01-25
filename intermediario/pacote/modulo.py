81121f2ccdc120b884b8f7c06436e48297d84ade
Ruan Araújo <ruands2002@gmail.com>
2024-01-25 00:03:36 -0300

    intermediário: introdução à pacotes-a

---
+++ intermediario/pacote/modulo.py
@@ -0,0 +1,5 @@
+print('Inicializando módulo:', __name__)
+
+
+def soma_do_modulo(x, y):
+    return x + y
---
+++ intermediario/pacotes.py
@@ -0,0 +1,7 @@
+import pacote.modulo
+from pacote import modulo
+from pacote.modulo import soma_do_modulo
+
+print(soma_do_modulo(1, 2))
+print(pacote.modulo.soma_do_modulo(1, 2))
+print(modulo.soma_do_modulo(1, 2))
