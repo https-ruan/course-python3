0031450808aa7f9d5ceced4ec017e14151e1090f
Ruan Araújo <ruands2002@gmail.com>
2024-01-31 22:40:58 -0300

    poo: método vs @classmethod vs @staticmethod

---
+++ poo/classmethod-vs-staticmethod.py
@@ -0,0 +1,37 @@
+# method vs @classmethod vs @staticmethod
+# method - self, método de instância
+# @classmethod - cls, método de classe
+# @staticmethod - método estático (❌self, ❌cls)
+class Connection:
+    def __init__(self, host='localhost'):
+        self.host = host
+        self.user = None
+        self.password = None
+
+    def set_user(self, user):
+        # setter
+        self.user = user
+
+    def set_password(self, password):
+        # setter
+        self.password = password
+
+    @classmethod
+    def create_user_auth(cls, user, password):
+        connection = cls()
+        connection.user = user
+        connection.password = password
+        return connection
+
+    @staticmethod
+    def log(msg):
+        print('LOG:', msg)
+
+
+# c1 = Connection()
+# c1.set_user('ruan')
+# c1.set_password('123')
+c1 = Connection.create_user_auth('ruan', '1234')
+print(Connection.log('Essa é a mensagem de log'))
+# print(c1.user)
+# print(c1.password)
