7be1f085ab2ee95ac23588129f27048ec6c19a89
Ruan Araújo <ruands2002@gmail.com>
2024-02-07 15:35:49 -0300

    poo: encapsulamento (modificadores de acesso)

---
+++ poo/encapsulamento.py
@@ -0,0 +1,36 @@
+# Encapsulamento (modificadores de acesso: public, protected, private)
+# Python NÃO TEM modificadores de acesso
+# Mas podemos seguir as seguintes convenções
+#   (sem underline) = public
+#       pode ser usado em qualquer lugar
+# _ (um underline) = protected
+#       não DEVE ser usado fora da classe
+#       ou suas subclasses.
+# __ (dois underlines) = private
+#       "name mangling" (desfiguração de nomes) em Python
+#       _NomeClasse__nome_attr_ou_method
+#       só DEVE ser usado na classe em que foi
+#       declarado.
+from functools import partial
+
+
+class Foo:
+    def __init__(self):
+        self.public = 'Isso é publico'
+        self._protected = 'Isso é protegido'
+        self.__private = 'Isso é privado'
+
+    def metodo_public(self):
+        print(self.__private)
+        return 'metodo_publico'
+
+    def _metodo_protected(self):
+        return '_metodo_protected'
+
+
+f = Foo()
+# print(f.public)
+# print(f.metodo_public())
+# print(f._protected)
+# print(f._metodo_protected())
+print(f._Foo__private)
