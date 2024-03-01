8a136d35e0d714d5d7c5bfda520d898f54cad0db
Ruan Araújo <ruands2002@gmail.com>
2024-03-01 19:30:13 -0300

    poo: exemplo de uso dos dunder methods

--- poo/dunder-methods.py
+++ poo/dunder-methods.py
@@ -21,17 +21,29 @@ class Ponto:
         self.y = y
         self.z = z
 
-    def __str__(self) -> str:
-        return f'{self.x}, {self.y}'
-
     def __repr__(self) -> str:
         # class_name = self.__class__.__name__
         class_name = type(self).__name__
         return f'{class_name}(x={self.x!r}, y={self.y!r}, z={self.z!r})'
 
+    def __add__(self, other):
+        novo_x = self.x + other.x
+        novo_y = self.y + other.y
+        return Ponto(novo_x, novo_y)
 
-p1 = Ponto(1, 2)
-p2 = Ponto(3, 4)
-print(p1.__str__())
-print(repr(p2))
-print(f'{p2!r}')
+    def __gt__(self, other):
+        resultado_self = self.x + self.y
+        resultado_other = other.x + other.y
+        return resultado_self > resultado_other
+
+
+if __name__ == '__main__':
+    p1 = Ponto(1, 2)  # 3
+    p2 = Ponto(3, 4)  # 7
+    p3 = p1 + p2
+    # print(p1.__str__())
+    # print(repr(p2))
+    # print(f'{p2!r}')
+    print(p3)
+    print('P1 é maior que P2', p1 > p2)
+    print('P2 é maior que P1', p2 > p1)
