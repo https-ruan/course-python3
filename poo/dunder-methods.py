39d1088eff3ae4795e5c3321b61fa44c1b7e78f7
Ruan Araújo <ruands2002@gmail.com>
2024-03-01 19:13:37 -0300

    poo: __repr__ e __str__

--- poo/dunder-methods.py
+++ poo/dunder-methods.py
@@ -15,3 +15,23 @@
 # __neg__(self) - -self
 # __str__(self) - str
 # __repr__(self) - str
+class Ponto:
+    def __init__(self, x, y, z='string') -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self) -> str:
+        return f'{self.x}, {self.y}'
+
+    def __repr__(self) -> str:
+        # class_name = self.__class__.__name__
+        class_name = type(self).__name__
+        return f'{class_name}(x={self.x!r}, y={self.y!r}, z={self.z!r})'
+
+
+p1 = Ponto(1, 2)
+p2 = Ponto(3, 4)
+print(p1.__str__())
+print(repr(p2))
+print(f'{p2!r}')
