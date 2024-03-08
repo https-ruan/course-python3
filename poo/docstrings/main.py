2d09e0af9cc31f6ffad369be154f07487e33f501
Ruan Araújo <ruands2002@gmail.com>
2024-03-08 16:49:36 -0300

    docstring: documentação de uma linha

---
+++ poo/docstrings/main.py
@@ -0,0 +1,7 @@
+import uma_linha
+
+print(dir(uma_linha))  # retorna tudo que o módulo possui
+print(uma_linha.__doc__)  # retorna a documentação inicial do módulo
+print(uma_linha.__file__)  # retorna caminho do módulo
+print(uma_linha.__name__)  # retorna nome do módulo
+help(uma_linha)
---
+++ poo/docstrings/uma_linha.py
@@ -0,0 +1,1 @@
+""" O que seu módulo faz """
