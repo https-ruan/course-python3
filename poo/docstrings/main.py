b15f09a81926184734280d3aa7d2132976bb7fe9
Ruan Araújo <ruands2002@gmail.com>
2024-03-08 16:57:41 -0300

    docstrings: documentação de várias linhas

--- poo/docstrings/main.py
+++ poo/docstrings/main.py
@@ -1,7 +1,10 @@
-import uma_linha
+# import uma_linha
 
-print(dir(uma_linha))  # retorna tudo que o módulo possui
-print(uma_linha.__doc__)  # retorna a documentação inicial do módulo
-print(uma_linha.__file__)  # retorna caminho do módulo
-print(uma_linha.__name__)  # retorna nome do módulo
-help(uma_linha)
+# print(dir(uma_linha))  # retorna tudo que o módulo possui
+# print(uma_linha.__doc__)  # retorna a documentação inicial do módulo
+# print(uma_linha.__file__)  # retorna caminho do módulo
+# print(uma_linha.__name__)  # retorna nome do módulo
+# help(uma_linha)
+
+import varias_linhas
+help(varias_linhas)
