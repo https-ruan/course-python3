a9f1acf3ac6e60e07e5a8d6d39b30fda900447a1
Ruan Araújo <ruands2002@gmail.com>
2024-03-08 16:35:31 -0300

    poo: __new__ de uma metaclass

--- poo/metaclasses.py
+++ poo/metaclasses.py
@@ -28,8 +28,48 @@
 #     ...
 
 
-Foo = type('Foo', (object,), {})
-f = Foo()
-# print(isinstance(f, Foo))
-print(type(f))
-print(type(Foo))
+# Foo = type('Foo', (object,), {})
+# f = Foo()
+# # print(isinstance(f, Foo))
+# print(type(f))
+# print(type(Foo))
+
+# * __new__ de uma metaclass -> cria e retorna a classe em si
+def meu_repr(self):
+    return f'{type(self).__name__}({self.__dict__})'
+
+
+class Meta(type):
+    def __new__(mcs, name, bases, dct):
+        print('METACLASS NEW')
+        cls = super().__new__(mcs, name, bases, dct)
+        cls.attr = 1234
+        cls.__repr__ = meu_repr
+
+        if 'falar' not in cls.__dict__ or \
+                not callable(cls.__dict__['falar']):
+            raise NotImplemented('Implemente falar')
+
+        return cls
+
+
+class Pessoa(object, metaclass=Meta):
+    # falar = 1234
+
+    def __new__(cls, *args, **kwargs):
+        print('MEU NEW')
+        instancia = super().__new__(cls)
+        return instancia
+
+    def __init__(self, nome):
+        print('MEU INIT')
+        self.nome = nome
+
+    def falar(self):
+        print('FALANDO...')
+
+
+p1 = Pessoa('Ruan')
+print(p1.attr)
+print(Pessoa.attr)
+print(p1)
