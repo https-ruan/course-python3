3c482c102310fba33bdd159d8f132cea679acef1
Ruan Araújo <ruands2002@gmail.com>
2024-03-27 12:34:39 -0300

    modulos py: escrevendo arquivos csv

---
+++ modulos-python/mod-csv/writer-dictwriter.py
@@ -0,0 +1,35 @@
+# csv.writer e csv.DictWriter para escrever em CSV
+# csv.reader lê o CSV em formato de lista
+# csv.DictReader lê o CSV em formato de dicionário
+import csv
+from dataclasses import field
+from pathlib import Path
+
+CAMINHO_CSV = Path(__file__).parent / 'example2.csv'
+
+lista_clientes = [
+    {'Nome': 'Luiz Otávio', 'Endereço': 'Av 1, 22'},
+    {'Nome': 'João Silva', 'Endereço': 'R. 2, "1"'},
+    {'Nome': 'Maria Sol', 'Endereço': 'Av B, 3A'},
+]
+
+# with open(CAMINHO_CSV, 'w', encoding='utf8') as arquivo:
+#     nome_colunas = lista_clientes[0].keys()
+#     escritor = csv.writer(arquivo)
+
+#     escritor.writerow(nome_colunas)
+
+#     for cliente in lista_clientes:
+#         escritor.writerow(cliente.values())
+
+with open(CAMINHO_CSV, 'w', encoding='utf8') as arquivo:
+    nome_colunas = lista_clientes[0].keys()
+    escritor = csv.DictWriter(
+        arquivo,
+        fieldnames=nome_colunas
+    )
+
+    escritor.writeheader()
+
+    for cliente in lista_clientes:
+        escritor.writerow(cliente)
