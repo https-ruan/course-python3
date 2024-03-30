93b2a15a5318070cd35ef8fc244efe4f06622f0b
Ruan Araújo <ruands2002@gmail.com>
2024-03-30 10:41:30 -0300

    modulos py: selenium -> buscando e utilizando elementos da pagina

--- modulos-python/mod-selenium/chromedriver.py
+++ modulos-python/mod-selenium/chromedriver.py
@@ -2,8 +2,11 @@
 from pathlib import Path
 from time import sleep
 
-from selenium import webdriver
+from selenium import webdriver  # type: ignore
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 # Chrome Options
 # https://peter.sh/experiments/chromium-command-line-switches/
@@ -21,7 +24,7 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
     # chrome_options.add_argument('--headless')
     if options is not None:
         for option in options:
-            chrome_options.add_argument(option)  # type: ignore
+            chrome_options.add_argument(option)
 
     chrome_service = Service(
         executable_path=str(CHROME_DRIVER_PATH),
@@ -36,6 +39,8 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
 
 
 if __name__ == '__main__':
+    TIME_TO_WAIT = 10
+
     # Example
     # options = '--headless', '--disable-gpu',
     options = ()
@@ -44,5 +49,15 @@ if __name__ == '__main__':
     # Como antes
     browser.get('https://www.google.com')
 
+    # Espere para encontrar o input
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+        EC.presence_of_element_located(
+            (By.NAME, 'q')
+        )
+    )
+
+    search_input.send_keys('Hello World!')
+    search_input.submit()
+
     # Dorme por 10 segundos
-    sleep(10)
+    sleep(TIME_TO_WAIT)
