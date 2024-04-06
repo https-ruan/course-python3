b457d63feb94d33591bd356b8b4837e47f119f88
Ruan Araújo <ruands2002@gmail.com>
2024-04-05 21:27:45 -0300

    modulos py: threads #2

--- modulos-python/threads/threads.py
+++ modulos-python/threads/threads.py
@@ -1,28 +1,72 @@
-from threading import Thread
+from threading import Lock, Thread
 from time import sleep
 
+# class MeuThread(Thread):
+#     def __init__(self, texto, tempo):
+#         self.texto = texto
+#         self.tempo = tempo
 
-class MeuThread(Thread):
-    def __init__(self, texto, tempo):
-        self.texto = texto
-        self.tempo = tempo
+#         super().__init__()
 
-        super().__init__()
-
-    def run(self):
-        sleep(self.tempo)
-        print(self.texto)
+#     def run(self):
+#         sleep(self.tempo)
+#         print(self.texto)
 
 
-t1 = MeuThread('Thread 1', 2)
-t1.start()
+# t1 = MeuThread('Thread 1', 2)
+# t1.start()
 
-t2 = MeuThread('Thread 2', 3)
-t2.start()
+# t2 = MeuThread('Thread 2', 3)
+# t2.start()
 
-t3 = MeuThread('Thread 3', 5)
-t3.start()
+# t3 = MeuThread('Thread 3', 5)
+# t3.start()
 
-for i in range(20):
-    print(i)
-    sleep(1)
+# for i in range(20):
+#     print(i)
+#     sleep(1)
+
+# def vai_demorar(texto, tempo):
+#     sleep(tempo)
+#     print(texto)
+
+
+# t1 = Thread(target=vai_demorar, args=('Olá mundo', 5))
+# t1.start()
+
+# while t1.is_alive():
+#     print('Esperando a thread.')
+#     sleep(2)
+
+# print('Thread acabou!')
+
+class Ingressos:
+    def __init__(self, estoque) -> None:
+        self.estoque = estoque
+        self.lock = Lock()
+
+    def comprar(self, quantidade):
+        self.lock.acquire()
+
+        if self.estoque < quantidade:
+            print('Não temos ingressos suficientes.')
+            self.lock.release()
+            return
+
+        sleep(1)
+        self.estoque -= quantidade
+
+        print(f'Você comprou {quantidade} ingressos. '
+              f'Ainda temos {self.estoque} em estoque.')
+
+        self.lock.release()
+
+
+if __name__ == '__main__':
+    ingressos = Ingressos(10)
+
+    for i in range(1, 12):
+        t = Thread(target=ingressos.comprar, args=(i,))
+        t.start()
+
+    print(ingressos.estoque)
