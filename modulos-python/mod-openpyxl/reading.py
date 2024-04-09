8837804bba66cd011865a30630dd3539d6444539
Ruan Araújo <ruands2002@gmail.com>
2024-04-08 22:38:40 -0300

    modulos py: openpyxl #2

--- modulos-python/mod-openpyxl/reading.py
+++ modulos-python/mod-openpyxl/reading.py
@@ -10,40 +10,27 @@
 
 from pathlib import Path
 
-from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.cell.cell import Cell
 from openpyxl.worksheet.worksheet import Worksheet
 
 ROOT_FOLDER = Path(__file__).parent
 WORKBOOK_PATH = ROOT_FOLDER / 'workbook.xlsx'
 
-workbook = Workbook()
+workbook = load_workbook(WORKBOOK_PATH)
 sheet_name = 'Minha planilha'
-workbook.create_sheet(sheet_name, 0)
+
 worksheet: Worksheet = workbook[sheet_name]
 
-# Remover uma planilha
-workbook.remove(workbook['Sheet'])
+row: tuple[Cell]
+for row in worksheet.iter_rows(min_row=2):
+    for cell in row:
+        print(cell.value, end='\t')
 
-print(workbook.sheetnames)
+        if cell.value == 'Maria':
+            worksheet.cell(cell.row, 2, 23)
+    print()
 
-# Criando os cabeçalhos
-worksheet.cell(1, 1, 'Nome')
-worksheet.cell(1, 2, 'Idade')
-worksheet.cell(1, 3, 'Nota')
-
-students = [
-    # nome  idade nota
-    ['João', 14, 5.5],
-    ['Maria', 13, 9.7],
-    ['Luiz', 15, 8.7],
-    ['Alberto', 16, 10],
-]
-
-# for i, student_row in enumerate(students, start=2):
-#     for j, student_column in enumerate(student_row, start=1):
-#         worksheet.cell(i, j, student_column)
-
-for student in students:
-    worksheet.append(student)
+# worksheet['B3'].value = 14
 
 workbook.save(WORKBOOK_PATH)
