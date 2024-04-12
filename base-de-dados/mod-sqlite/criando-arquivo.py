eb0a509a2fb80a5c73c07f6de86532772f1c9c20
Ruan Araújo <ruands2002@gmail.com>
2024-04-12 20:29:12 -0300

    db: criando tabela sqlite

--- base-de-dados/mod-sqlite/criando-arquivo.py
+++ base-de-dados/mod-sqlite/criando-arquivo.py
@@ -8,10 +8,21 @@ DB_FILE = DB_FOLDER / DB_NAME
 
 Path.mkdir(DB_FOLDER, exist_ok=True)
 
+TABLE_NAME = 'customers'
+
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
-# SQL
+# Cria a tabela
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
 
 cursor.close()
 connection.close()
