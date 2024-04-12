155478ac923b77d4633187d9479e10a8de1a16a8
Ruan Araújo <ruands2002@gmail.com>
2024-04-12 20:38:42 -0300

    db(sqlite): inserindo dados

--- base-de-dados/mod-sqlite/criando-arquivo.py
+++ base-de-dados/mod-sqlite/criando-arquivo.py
@@ -13,6 +13,10 @@ TABLE_NAME = 'customers'
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+# CUIDADO: fazendo delete sem where
+cursor.execute(f'DELETE FROM {TABLE_NAME}')
+cursor.execute(f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"')
+
 # Cria a tabela
 cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
@@ -21,6 +25,15 @@ cursor.execute(
     'name TEXT,'
     'weight REAL'
     ')'
+)
+connection.commit()
+
+# Registrar valores nas colunas da tabela
+# CUIDADO: sql injection
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} (id, name, weight) '
+    'VALUES '
+    '(NULL, "Ruan Araujo", 85), (NULL, "Helena", 4.3)'
 )
 connection.commit()
 
